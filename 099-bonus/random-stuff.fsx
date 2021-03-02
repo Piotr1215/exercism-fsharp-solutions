5141bfca2a8d15a165a2036fc676b809997f279d
decoder <decoder@lilve.de>
2021-03-03 00:49:58 +0100

    bonus material

--- 099-bonus/random-stuff.fsx
+++ 099-bonus/random-stuff.fsx
@@ -1,7 +1,12 @@
 open System
 
 // Functions can have names with spaces if enclosed with double back ticks
-let repeatStr n (s: string) = Enumerable
+let sum (x:int, y:int) :int = x + y
+let multiply (x:int, y:int) :int = (x:int)*(y:int)
+
+let x = printfn "%i" x
+
+let smulte =
+    sum >> multiply >> x
 
 
-summation 8
